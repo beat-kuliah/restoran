0234ee8fed3f255b8eede9c991206ea568517b26
Fransiskus <fransiskusfernando095@gmail.com>
2024-07-27 21:38:32 +0700

    secure

--- resources/views/Customer/table.blade.php
+++ resources/views/Customer/table.blade.php
@@ -6,7 +6,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Pilih Meja</title>
         <script type="text/javascript" src="/js/app.js"></script>
