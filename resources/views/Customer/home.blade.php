7d71d1120946250b1539594b8f8fe0cd1efa8bd2
Fransiskus <fransiskusfernando095@gmail.com>
2024-07-27 21:32:44 +0700

    secure

--- resources/views/Customer/home.blade.php
+++ resources/views/Customer/home.blade.php
@@ -6,7 +6,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="/css/app.css" rel="stylesheet">
     <script type="text/javascript" src="/js/app.js"></script>
     <title>Home</title>
